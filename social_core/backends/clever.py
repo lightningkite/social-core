54744739842628a7001102864362e04d352592a9
garryjable <garrettjamesjennings@gmail.com>
2020-10-23 11:24:34 -0600

    lotsa ipdb

--- social_core/backends/clever.py
+++ social_core/backends/clever.py
@@ -2,7 +2,6 @@ import base64
 
 from .oauth import BaseOAuth2
 
-this is a syntax error
 class CleverOAuth2(BaseOAuth2):
     """
     Clever OAuth2 authentication backend
@@ -20,6 +19,7 @@ class CleverOAuth2(BaseOAuth2):
     REDIRECT_STATE = False
 
     def user_data(self, access_token, *args, **kwargs):
+        import ipdb; ipdb.set_trace()
         response = self.get_json(
             self.USER_DETAILS_URL, headers={
                 'Authorization': 'Bearer {access_token}'.format(access_token=access_token)
@@ -33,6 +33,7 @@ class CleverOAuth2(BaseOAuth2):
         return response
 
     def get_user_details(self, response):
+        import ipdb; ipdb.set_trace()
         username = response.get('id', '')
         first_name = response.get('first_name', '')
         last_name = response.get('last_name', '')
@@ -46,15 +47,17 @@ class CleverOAuth2(BaseOAuth2):
             'last_name': last_name,
         }
 
-    #def auth_complete_params(self, state=None):
-    #    state["client_id"] = '5a7a8821db09037d6082'
-    #    return {
-    #        'grant_type': 'authorization_code',  # request auth code
-    #        'code': self.data.get('code', ''),  # server response code
-    #        'redirect_uri': self.get_redirect_uri(state),
-    #    }
+    def auth_complete_params(self, state=None):
+        import ipdb; ipdb.set_trace()
+        state["client_id"] = '5a7a8821db09037d6082'
+        return {
+            'grant_type': 'authorization_code',  # request auth code
+            'code': self.data.get('code', ''),  # server response code
+            'redirect_uri': self.get_redirect_uri(state),
+        }
 
     def auth_headers(self):
+        import ipdb; ipdb.set_trace()
         return {
             'Authorization': b'Basic ' + base64.urlsafe_b64encode(
                 '{0}:{1}'.format(*self.get_key_and_secret()).encode()
@@ -62,4 +65,5 @@ class CleverOAuth2(BaseOAuth2):
         }
 
     def refresh_token_params(self, token, *args, **kwargs):
+        import ipdb; ipdb.set_trace()
         return {'refresh_token': token, 'grant_type': 'refresh_token'}
